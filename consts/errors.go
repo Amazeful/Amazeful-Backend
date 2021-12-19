d0ed5c85d03e80a9f1f1bdecc7fa16eea3ea4971
Aiden Hadisi <so.hadisi@gmail.com>
2021-12-18 20:44:32 -0800

    fix error messages

--- consts/errors.go
+++ consts/errors.go
@@ -1,8 +1,8 @@
 package consts
 
 const (
-	ErrStrRetrieveData = "failed to retrieve data from database"
-	ErrStrDecode       = "failed to decode request data"
-	ErrStrInsert       = "failed to insert data into database"
-	ErrStrUnauthorized = "user is not authorized to view this resource"
+	ErrStrRetrieveData = "Failed to retrieve data from database."
+	ErrStrDecode       = "Failed to decode request data."
+	ErrStrInsert       = "Failed to insert data into database."
+	ErrStrUnauthorized = "User is not authorized to access this resource."
 )
